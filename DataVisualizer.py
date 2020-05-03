48a8fc98dec5bf47b4199ffb1877058cae0d2fae
Anton Kindt <anton.kindt@ugent.be>
2020-05-03 21:49:22 +0200

    almost beta edition

--- DataVisualizer.py
+++ DataVisualizer.py
@@ -6,19 +6,22 @@ class DataVisualizer:
     SUBTITLE = 2
     TEXT = 3
 
-    def __init__(self, font, x=0, y=0, marginLeft=20):
+    def __init__(self, font, width=300, height=300, x=0, y=0, marginLeft=20, colWidth = 120):
         self.font = font
         self.marginLeft = marginLeft
         self.x = x
         self.y = y
         self.yPos = y
         self.data = []
+        self.width = width
+        self.height = height
+        self.colWidth = colWidth
 
     def add(self, text, size, r=255, g=255, b=255):
         self.data.append({"text": text, "size": size, "red": r, "green": g, "blue": b})
 
     def display(self, frame):
-        cv.rectangle(frame, (self.x, self.y), (300 + self.x, 300 + self.y), (0, 0, 0), -1)
+        cv.rectangle(frame, (self.x, self.y), (self.width + self.x, self.height + self.y), (20, 20, 20), -1)
         for info in self.data:
             size = 0
             if info["size"] == self.HEADER:
@@ -37,9 +40,11 @@ class DataVisualizer:
             for col in info["text"]:
                 cv.putText(frame, col, (posx, self.yPos), self.font, size,
                            (info["blue"], info["green"], info["red"]), int(size + 0.5), cv.LINE_AA)
-                posx += 120
-
+                posx += self.colWidth
             if info["size"] == self.HEADER:
+                self.yPos += 10
+
+            if info["size"] == self.SUBTITLE:
                 self.yPos += 10
 
         self.data = []
--- run.py
+++ run.py
@@ -7,7 +7,7 @@ import numpy as np
 
 import RatUtils
 from progress.bar import Bar
-from PIL import ImageGrab, ImageFont
+from PIL import ImageGrab, Image
 
 from DataVisualizer import DataVisualizer
 
@@ -20,7 +20,7 @@ BLACK_THRESHOLD = 5
 # Checks the pixels relative to the top center of the video [row, col]
 # More pixels = less false detections
 PIXELS_TO_CHECK = [[30, -50], [30, 50]]
-# PIXELS_TO_CHECK = [[530, -350], [525, -300], [530, -250], [535, -200], [530, -150], [525, -100], [530, -50], [535, 0], [530, 50], ]
+#PIXELS_TO_CHECK = [[530, -350], [525, -300], [530, -250], [535, -200], [530, -150], [525, -100], [530, -50], [535, 0], [530, 50], ]
 
 # Show the video
 SHOW_VIDEO = True
@@ -69,6 +69,10 @@ outputLocation = ""
 
 # Fullscreen (this reduces the speed dramatically)
 FULLSCREEN = False
+
+# Border
+BORDER_LEFT = 400
+BORDER_BOTTOM = 40
 
 #   END OF SETTINGS
 
@@ -119,8 +123,8 @@ cap = cv.VideoCapture()
 cap.open(video)
 cap.set(cv.CAP_PROP_POS_FRAMES, BEGIN_FRAME)
 
-width = int(cap.get(cv.CAP_PROP_FRAME_WIDTH))
-height = int(cap.get(cv.CAP_PROP_FRAME_HEIGHT))
+width = int(cap.get(cv.CAP_PROP_FRAME_WIDTH)) + BORDER_LEFT
+height = int(cap.get(cv.CAP_PROP_FRAME_HEIGHT)) + BORDER_BOTTOM
 
 frameCounter = 0
 loadingFrameCounter = 0
@@ -129,30 +133,26 @@ if END_FRAME == -1:
     END_FRAME = cap.get(cv.CAP_PROP_FRAME_COUNT)
 totalFrames = END_FRAME - BEGIN_FRAME
 
-dataVisualizer = DataVisualizer(FONT)
-
+dataVisualizer = DataVisualizer(FONT, width=width, height=BORDER_BOTTOM, x=0, y=height - BORDER_BOTTOM, colWidth=180)
 out = 0
 if OUTPUT:
     out = cv.VideoWriter(outputLocation, cv.VideoWriter_fourcc(*'XVID'), frameRate, (width, height))
 
 
 def display():
-    if SHOW_BLACK_PIXEL_DETECTORS:
-        RatUtils.showDetectors(frame, PIXELS_TO_CHECK, width)
-    dataVisualizer.add(["Ratatool"], DataVisualizer.HEADER)
-    dataVisualizer.add(["frame: {}/{}".format(frameCounter, totalFrames)], DataVisualizer.TEXT)
-    dataVisualizer.add(["{} loading frames".format(loadingFrameCounter)], DataVisualizer.TEXT)
-    dataVisualizer.add(["{}%".format(round(loadingFrameCounter / frameCounter * 100, 2))], DataVisualizer.TEXT)
-    dataVisualizer.add(["framerate: {}".format(frameRate)], DataVisualizer.TEXT)
-    minutes, seconds = RatUtils.timeCalc(frameCounter // frameRate)
-    dataVisualizer.add(["realtime: {:02d}:{:02d}".format(minutes, seconds)], DataVisualizer.TEXT)
-    minutes, seconds = RatUtils.timeCalc((frameCounter - loadingFrameCounter) // frameRate)
-    dataVisualizer.add(["without loads: {:02d}:{:02d}".format(minutes, seconds)], DataVisualizer.TEXT)
+    rm, rs = RatUtils.timeCalc(frameCounter // frameRate)
+    wm, ws = RatUtils.timeCalc((frameCounter - loadingFrameCounter) // frameRate)
+    dataVisualizer.add(["frame: {}/{}".format(frameCounter, totalFrames),
+                        "{} loading frames".format(loadingFrameCounter),
+                        "{}%".format(round(loadingFrameCounter / frameCounter * 100, 2)),
+                        "framerate: {}".format(frameRate),
+                        "realtime: {:02d}:{:02d}".format(rm, rs),
+                        "without loads: {:02d}:{:02d}".format(wm, ws)], DataVisualizer.TEXT)
     dataVisualizer.display(frame)
 
 
 if SPLITS:
-    splitVisualizer = DataVisualizer(FONT, y=400)
+    splitVisualizer = DataVisualizer(FONT, y=0)
     speed = frameRate
     activeSplit = 0
     while True:
@@ -181,27 +181,29 @@ if SPLITS:
         SPLIT_POINTS[activeSplit][1] = frameCounter
         splitVisualizer.add(["Splits"], DataVisualizer.HEADER)
         splitVisualizer.add(["Speed: " + str(round(speed / frameRate, 2)) + " seconds"], DataVisualizer.TEXT)
+
         for split in SPLIT_POINTS:
             minutes, seconds = RatUtils.timeCalc(split[1] // frameRate)
+            r, g, b = 255, 255, 255
             if split is SPLIT_POINTS[activeSplit]:
-                splitVisualizer.add([split[0], "{:d}:{:02d}".format(minutes, seconds)], DataVisualizer.TEXT, r=0,
-                                    g=255, b=255)
-            else:
-                splitVisualizer.add([split[0], "{:d}:{:02d}".format(minutes, seconds)], DataVisualizer.TEXT)
+                r = 0
+            splitVisualizer.add([split[0], "{:d}:{:02d}".format(minutes, seconds)], DataVisualizer.TEXT, r=r, g=g, b=b)
+
         cap.set(cv.CAP_PROP_POS_FRAMES, frameCounter + BEGIN_FRAME)
         ret, frame = cap.read()
+        if SHOW_BLACK_PIXEL_DETECTORS:
+            RatUtils.showDetectors(frame, PIXELS_TO_CHECK, width)
         splitVisualizer.display(frame)
         cv.imshow("splitfinder", frame)
 
     cap.set(cv.CAP_PROP_POS_FRAMES, BEGIN_FRAME)
-    cv.destroyWindow('splitfinder')
+    cv.destroyWindow("splitfinder")
     frameCounter = 0
 
 progressBar = Bar('Processing', max=totalFrames // PROGRESS_BAR_FRAME_SKIPS, suffix='%(index)d/%(max)d - %('
                                                                                     'percent).1f%% - %(eta)ds')
 
 activeSplit = 0
-
 while True:
     frameCounter += 1
     if LIVE_MODE:
@@ -209,38 +211,52 @@ while True:
         frame = cv.resize(np.array(printscreen_pil, dtype="uint8"), (0, 0), fx=0.2, fy=0.2)
     else:
         ret, frame = cap.read()
-
-    if RatUtils.isLoadingFrame(frame, PIXELS_TO_CHECK, BLACK_THRESHOLD, width):
+    if RatUtils.isLoadingFrame(frame, PIXELS_TO_CHECK, BLACK_THRESHOLD, width - BORDER_LEFT):
         loadingFrameCounter += 1
+    if SHOW_BLACK_PIXEL_DETECTORS:
+        RatUtils.showDetectors(frame, PIXELS_TO_CHECK, width - BORDER_LEFT)
+    frame = cv.copyMakeBorder(frame, 0, BORDER_BOTTOM, BORDER_LEFT, 0, cv.BORDER_CONSTANT, (0, 0, 0))
 
     if SPLITS:
-        sv = DataVisualizer(FONT, y=400)
-        sv.add(["Splits"], DataVisualizer.HEADER)
+        sv = DataVisualizer(FONT, y=0, width=BORDER_LEFT, height=height - BORDER_BOTTOM)
+        sv.add(["Splits"], DataVisualizer.SUBTITLE)
+        sv.add(["split", "with loads", "without loads"], DataVisualizer.TEXT)
         for split in SPLIT_POINTS:
             if split[1] < frameCounter:
-                minutes, seconds = RatUtils.timeCalc(split[1] // frameRate)
+                rm, rs = RatUtils.timeCalc(split[1] // frameRate)
+                wm, ws = RatUtils.timeCalc((split[1] - loadingFrameCounter) // frameRate)
                 r, g, b = 0, 255, 0
             elif split is SPLIT_POINTS[activeSplit]:
-                minutes, seconds = RatUtils.timeCalc(frameCounter // frameRate)
+                rm, rs = RatUtils.timeCalc(frameCounter // frameRate)
+                wm, ws = RatUtils.timeCalc((frameCounter - loadingFrameCounter) // frameRate)
                 r, g, b = 0, 255, 255
             else:
+                rm, rs = 0, 0
+                wm, ws = 0, 0
                 r, g, b = 200, 200, 200
-                minutes, seconds = RatUtils.timeCalc(0)
-            sv.add([split[0], "{:d}:{:02d}".format(minutes, seconds)], DataVisualizer.TEXT, r=r, g=g, b=b)
+            sv.add([split[0], "{:d}:{:02d}".format(rm, rs), "{:d}:{:02d}".format(wm, ws)], DataVisualizer.TEXT, r=r,
+                   g=g, b=b)
         if SPLIT_POINTS[activeSplit][1] == frameCounter:
             activeSplit += 1
         if activeSplit > len(SPLIT_POINTS) - 1:
             break
         sv.display(frame)
 
+    display()
     if VISUAL:
-        display()
         if FULLSCREEN:
             cv.namedWindow("ratatool", cv.WND_PROP_FULLSCREEN)
             cv.setWindowProperty("ratatool", cv.WND_PROP_FULLSCREEN, cv.WINDOW_FULLSCREEN)
         cv.imshow("ratatool", frame)
-        if OUTPUT:
-            out.write(frame)
+    elif frameCounter%100 == 0:
+        loading = np.zeros((200, 300, 3), np.uint8)
+        loadingVisualizer = DataVisualizer(FONT, 200, 300)
+        loadingVisualizer.add(["loading..."], DataVisualizer.HEADER)
+        loadingVisualizer.add([str(round(frameCounter/totalFrames*100, 2))], DataVisualizer.TEXT)
+        loadingVisualizer.display(loading)
+        cv.imshow("loading", loading)
+    if OUTPUT:
+        out.write(frame)
     if frameCounter % PROGRESS_BAR_FRAME_SKIPS == 0:
         progressBar.next()
 
@@ -255,6 +271,8 @@ if VISUAL and frameCounter == END_FRAME - BEGIN_FRAME:
     cv.waitKey(0)
     cv.destroyWindow('ratatool')
 else:
+    if SHOW_BLACK_PIXEL_DETECTORS:
+        RatUtils.showDetectors(frame, PIXELS_TO_CHECK, width - BORDER_LEFT)
     display()
     while True:
         if cv.waitKey(1) & 0xFF == ord(EXIT_KEY):
